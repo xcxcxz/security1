e3154fb21cc4fb404df9f135d30694b65bfb518f
ZZoSuke <xcxcxz@naver.com>
2023-04-17 15:49:20 +0900

    :heavy_plus_sign: 로그인페이지 생성
    스프링 시큐리티내의 인터셉터 확인

---
+++ src/main/java/com/example/security1/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록됨.
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/user/**").authenticated()
+                .requestMatchers("/manager/**").hasAnyRole("ROLE_ADMIN", "ROLE_MANAGER")
+                .requestMatchers("/managet/**").hasRole("ROLE_ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and().build();
+
+
+    }
+
+
+
+}
